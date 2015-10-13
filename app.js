8d9c74202ce95d4f5da5881302b33da2921f7277
liuxin.rkl <liuxin.rkl@taobao.com>
2015-10-13 10:51:03 +0800

    fix tabs

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ var koa = require("koa");
 var app = koa();
 
 app.use(function *(next){
-	var start = new Date();
-	yield next;
-	var ms = new Date() - start;
-	this.set("X-Response-Time", ms + "ms");
+  var start = new Date();
+  yield next;
+  var ms = new Date() - start;
+  this.set("X-Response-Time", ms + "ms");
 });
 
 app.use(function *(next) {
-	var start = new Date();
-	yield next;
-	var ms = new Date() - start;
-	console.log("%s %s - %s", this.method, this.url, ms);
+  var start = new Date();
+  yield next;
+  var ms = new Date() - start;
+  console.log("%s %s - %s", this.method, this.url, ms);
 });
 
 app.use(function *(next) {
-	this.body = "hello world";
+  this.body = "hello world";
 });
 
 app.listen(3000);
